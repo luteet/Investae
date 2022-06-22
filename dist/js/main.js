d05ff46936b98de4c5a6006e6f3ff3b0d18da8c2
Roman Lukjanets <lukyanets14@gmail.com>
2022-06-22 16:40:50 +0300

    v1.1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,65 @@
 
+function galleryPopup(arg) {
+
+  let html = arg.html,
+      body = arg.body,
+      link = arg.link;
+
+  let galleryPopupBlock = 
+          `
+          <div class="_gallery-popup _hidden">
+              <div class="_gallery-popup-bg"></div>
+              <div class="_gallery-popup-body _gallery-popup-max">
+                  <div class="_gallery-popup-loading">
+                    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+                  </div>
+                  <button type="button" class="_gallery-popup-close-btn"></button>
+                  <img src="${link.href}" class="_gallery-popup-img" alt="Image not found">
+              </div>
+          </div>
+          
+          `;
+
+    body.insertAdjacentHTML('beforeend', galleryPopupBlock);
+    html.style.setProperty('--popup-padding', window.innerWidth - body.offsetWidth + 'px');
+    body.classList.add('_popup-active');
+    
+    let galleryPopup = document.querySelector('._gallery-popup');
+
+    //galleryPopup.style.setProperty('--width-image', widthImage);
+
+    setTimeout(function() {
+      galleryPopup.classList.remove('_hidden');
+    },200);
+
+
+
+    function removeGalleryPopup() {
+      galleryPopup.classList.add('_hidden');
+      setTimeout(function() {
+        body.removeChild(galleryPopup);
+        body.classList.remove('_popup-active');
+        html.style.setProperty('--popup-padding', '0px');
+      },200);
+    }
+
+
+
+    galleryPopup.querySelector('._gallery-popup-close-btn').addEventListener('click', function() {
+      removeGalleryPopup();
+    });
+    galleryPopup.querySelector('._gallery-popup-bg').addEventListener('click', function() {
+      removeGalleryPopup();
+    });
+
+
+
+}
+
+
 const body = document.querySelector('body'),
       menu = document.querySelectorAll('.header__burger, .header__nav, body'),
+      html = document.querySelector('html'),
       burger = document.querySelector('.header__burger');
 
 
@@ -69,16 +128,30 @@ body.addEventListener('click', function (event) {
 
     }
 
+
+
+    let galleryLink = thisTarget.closest('._gallery-link');
+    if(galleryLink) {
+      event.preventDefault();
+      galleryPopup({
+        link: galleryLink,
+        html: html,
+        body: body,
+      });
+    }
+
 })
 
 
-/* function activetoScoll() {
-  const scollElements = document.querySelectorAll('._active-to-scoll'),
-        offset = document.querySelector('.offset-check-js');
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
 
+  return {
+  top: box.top + pageYOffset,
+  left: box.left + pageXOffset
+  };
 
-} */
-
+}
 
 
 
@@ -86,6 +159,7 @@ body.addEventListener('click', function (event) {
 
 wow = new WOW({
 mobile:       false,
+
 })
 wow.init();
 
